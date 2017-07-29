7d4b67a5dcde97edabe9992eae7ffea61473c0ef
Ivan Krivonos <devbackend@yandex.ru>
2017-07-29 15:28:36 +1000

    Add Repository wrapper phpdoc

--- src/wrappers/Repository.php
+++ src/wrappers/Repository.php
@@ -3,7 +3,17 @@
 namespace Webhooker\wrappers;
 
 /**
+ * Wrapper for repository data.
  *
+ * @property string     $uuid
+ * @property string     $scm
+ * @property string     $website
+ * @property string     $name
+ * @property string     $full_name
+ * @property string     $type
+ * @property bool       $is_private
+ * @property User       $owner
+ * @property LinkList   $links
  *
  * @author Ivan Krivonos <devbackend@yandex.ru>
  */
